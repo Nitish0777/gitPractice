9a64947aef8fca93bfcbe0b50734176f2a8a7398
Mohit Khade <mohitkhade11@gmail.com>
2023-02-14 20:18:20 +0530

    test5

--- hello.cpp
+++ hello.cpp
@@ -3,5 +3,9 @@ using namespace std;
 
 int main(){
     cout << "Nitish " << endl;
+    cout << "Mohit is added" << endl;
+    cout<<"WELCOME TO MY YOUTUBE CHANNEL GAIS"<<endl;
+    cout<<"Aarul is gay"<<endl;
+    cout<<"test5"<<endl;
     return 0;
 }
